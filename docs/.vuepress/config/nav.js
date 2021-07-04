fc045a4e14a576b1ac99258329f22ee0dfdab608
丁文江 <dingwenjiang@bytedance.com>
2021-07-04 23:25:13 +0800

    feat: init doc

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,21 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '前端',
+    link: '/web/'
+  },
+  {
+    text: '收藏',
+    link: '/pages/beb6c0bd8a66cea6/',
+  },
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
+]
