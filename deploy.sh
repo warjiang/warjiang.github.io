0052ea12bbce610ce97c1be035930381bd39ce5f
丁文江 <dingwenjiang@bytedance.com>
2021-07-05 02:38:07 +0800

    feat: disable coding.net

--- deploy.sh
+++ deploy.sh
@@ -31,14 +31,14 @@ git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:dingwewnjiang/warjiang/warjiang.git
-else
-  codingUrl=https://warjiang:${CODING_TOKEN}@e.coding.net/warjiang/warjiang.git
-fi
-git add -A
-git commit -m "${msg}"
-git push -f $codingUrl master # 推送到coding
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:dingwewnjiang/warjiang/warjiang.git
+# else
+#   codingUrl=https://warjiang:${CODING_TOKEN}@e.coding.net/warjiang/warjiang.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
 
 cd -
 rm -rf docs/.vuepress/dist
